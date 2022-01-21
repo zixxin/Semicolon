cc89e8adaa9c82807af7204a1becba2169d62168
YoonSoye <yoonzoeym27@gmail.com>
2022-01-21 15:02:13 +0900

    [etc] problems 수정 및 ui 수정(divider)

--- ohyeahthat/lib/app.dart
+++ ohyeahthat/lib/app.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ohyeahthat/screen/home.dart';
-import 'package:ohyeahthat/screen/login.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 
 class App extends StatelessWidget{
